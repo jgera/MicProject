b122b0a182df8b8545e3d6de3283dd190049d4a8
Eirik <eirikwlegernes@hotmail.com>
2015-03-09 00:33:11 +0100

    prepped for dual clusters

--- MATLAB/Sandbox/onlineDetectionDualCluser.m
+++ MATLAB/Sandbox/onlineDetectionDualCluser.m
@@ -2,7 +2,7 @@ close all
 
 Fs = 48000;
 N = 64; % Frame size
-L = 100; % Resampling
+L = 1000; % Resampling
 V_SOUND = 340.0;
 
 
@@ -14,16 +14,14 @@ H.NumChannels = 3;
 H.DeviceDataType = '24-bit integer';
 H.OutputNumOverrunSamples = true;
 
-% Mic positions
+% Internal cluster design
 d = 0.024; % mic spacing in equilateral triangle
 r_12 = [d 0 0]';
 r_13 = [d/2 d*sqrt(3)/2 0]';
 
-
-% Storage for plots
-%main_storage = zeros(size(y));
-%high_storage = zeros(size(y));
-%detection = zeros(length(y),3);
+% Cluster positioning
+p_1 = []'; % Cluster 1 position
+p_2 = []'; % Cluster 2 position
 
 % Filters
 [b_main, a_main] = butter(4, 10000/(0.5*Fs),'high');
@@ -38,24 +36,28 @@ prevMain_dB = 0;
 mainAvg_dB = 0;
 mainAvg_alpha = 0.015*N/64;
 
-figure
 
 while true
     
     % Extract frame
     [frame , overrun] = step(H);
+    if overrun > 0
+       fprintf('Overrun\n'); 
+    end
 
     % Main tap highpass filter
     [frame_main, zi_main] = filter(b_main,a_main,frame,zi_main);
 
-    % Calculate levels
-    tap = 0;
+    % Split into two clusters
+    
+    
+    % Check levels
     main_dB = db(rms(frame_main));
     if main_dB - mainAvg_dB > minMain_dB
         if main_dB - prevMain_dB > minMainIncrease_dB
         
             % Tap detected
-
+            fprintf('Tap\n');
             % Resample and cross correlate
             frame_resampled = interpft(frame_main,N*L);
             maxlag = floor((0.024/340)*Fs*L);
@@ -74,24 +76,24 @@ while true
 
             A = [0 0 1];
             b = -1;
-            [v,resnorm,residual,exitflag,output,lambda] = lsqlin(C,d,A,b);
+            [v,resnorm,residual,exitflag,output,lambda] = lsqlin(C,d,[],[],A,b);
             v = v/norm(v);
             
             % Figure out the x y coordinates, at 1m, for demo use
             screenPoint = v(1:2)/v(3);
 
 
-            figure(1)
-            subplot(3,1,1)
-            plot(lags,r(:,2));
-            title('12');
-            subplot(3,1,2)
-            plot(lags,r(:,6));
-            title('23');
-            subplot(3,1,3)
-            plot(lags,r(:,7));
-            title('31');
-            drawnow
+            %figure(1)
+            %subplot(3,1,1)
+            %plot(lags,r(:,2));
+            %title('12');
+            %subplot(3,1,2)
+            %plot(lags,r(:,6));
+            %title('23');
+            %subplot(3,1,3)
+            %plot(lags,r(:,7));
+            %title('31');
+            %drawnow
 
             figure(2)
             scatter(screenPoint(1),screenPoint(2),'r');
